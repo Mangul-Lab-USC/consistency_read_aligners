c2061c356b246dd50b1e30666a3f0e7dd382ba7e
Yuen Wook Kim <dywk17@gmail.com>
2019-11-18 11:52:40 -0800

    Error Density

---
+++ error_density/buildRef.py
@@ -0,0 +1,50 @@
+# This import allows us to randomly pick a number.
+from random import randrange
+from Bio import SeqIO
+def batch_iterator(iterator, batch_size):
+    """Returns lists of length batch_size.
+
+    This can be used on any iterator, for example to batch up
+    SeqRecord objects from Bio.SeqIO.parse(...), or to batch
+    Alignment objects from Bio.AlignIO.parse(...), or simply
+    lines from a file handle.
+
+    This is a generator function, and it returns lists of the
+    entries from the supplied iterator.  Each list will have
+    batch_size entries, although the final list may be shorter.
+    """
+    entry = True  # Make sure we loop once
+    while entry:
+        batch = []
+        while len(batch) < batch_size:
+            try:
+                entry = next(iterator)
+            except StopIteration:
+                entry = None
+            if entry is None:
+                # End of file
+                break
+            batch.append(entry)
+        if batch:
+            yield batch
+# This is the list of bases
+bases = ["A", "C", "T", "G"]
+# This next line is to check whether a file "reference.fasta" exists. If it does, we clear it.
+referenceFile = open("reference.fasta", "w").close()
+# This line creates a "reference.fasta" file is it doesn't already exist. It then opens the file for writing.
+referenceFile = open("reference.fasta", "w+")
+# Here we write the first line of the "reference.fasta" file which is the identifier which starts with ">"
+referenceFile.write(">Reference\n")
+# Then using a for loop, we write the base pairs to the file. The range controls the number of base pairs we want.
+for i in range(1000000):
+    # This writes the base pairs to the file. The randrange(4) produces a random number between 0 and 4.
+    # We then take the base pair at that location in the bases list.
+    referenceFile.write(bases[randrange(4)])
+# This closes the reference file.
+referenceFile.close()
+iterator = SeqIO.parse(open("reference.fasta"),"fasta")
+for i, batch in enumerate(batch_iterator(iterator, 950)):
+    nameOfFile = "ref_%i.fasta" % (i + 1)
+    with open(nameOfFile, "w") as handle:
+        count = SeqIO.write(batch, handle, "fasta")
+    print("Wrote %i records to %s" % (count, nameOfFile))
--- error_density/readGenerator.py
+++ error_density/readGenerator.py
@@ -1,25 +1,37 @@
+# This allows us to pick random places in the reference to take reads from.
+from random import randrange
+
+# This opens the "reference.fasta" file for reading.
+referenceFile = open("reference.fasta", "r")
+# Just checking that we're in read mode.
+if referenceFile.mode == "r":
+    # We then read the information contained into a variable named reference.
+    reference = referenceFile.read()
+# Since we have the information we need, we can close the file.
+referenceFile.close()
+# These questions and inputs allows us to control the size and range of the reads.
+readLength = input("What is the length of the read that we want to see?")
+numErrors = input("What is the maximum number of errors that we want to see?")
+readLength = int(readLength)
+numErrors = int(numErrors)
+# This list stores the reads as we produce them in the next step
 # This for loop creates the reads. For i, a number starting at the readRangeStart mentioned,
 # and going all the way to readRangeEnd + readstep. This is because we want to include the max number not stop before it.
 # The reads are created and incremented in length by the readstep every step of the for loop.
-for i in range(readRangeStart, (readRangeEnd + readStep), readStep):
-    # This creates a starting point for the read. The 11 comes from the fact that we want to skip the identifier ">Reference".
-    # The 11 to 8500 range is a good place to start the read for references longer than 100000. Yet, the number should be changed based
-    # on the reference to make sure there aren't any issues accessing information that doesn't exist.
-    seqStart = randrange(11, 8500)
-    # The read length is going to be as long as i so we add the read length to get a end position.
-    seqEnd = seqStart + i;
-    # We take a substring of the reference genome to create the read using the range created in the above two lines.
-    reads.append(reference[seqStart: seqEnd])
+seqStart = randrange(11, 8500)
+seqEnd = seqStart + readLength;
+reads = [reads.append(reference[seqStart: seqEnd]) for i in range(numErrors)]
+print(reads)
 # This next line is to check whether a file "shortest.read.length.fasta" exists. If it does, we clear it.
 fastaShortReadsClear = open("shortest.read.length.fasta", "w").close()
 # This line creates a "shortest.read.length.fasta" file is it doesn't already exist. It then opens the file for writing.
 fastaShortReads = open("shortest.read.length.fasta", "w+")
 # This for loop writes the reads to the "shortest.read.length.fasta" file
-for i in range(len(reads)):
+for i in range(numErrors):
     # This is how identifiers start in fasta format
     fastaShortReads.write(">R")
     # This adds on to the identifier. We're using the format ">R(length of read)" to identify the reads.
-    fastaShortReads.write(str(len(reads[i])))
+    fastaShortReads.write(str(i))
     # Formatting fasta
     fastaShortReads.write("\n")
     # We write the read to the file.
@@ -33,11 +45,11 @@ fastqShortReadsClear = open("shortest.read.length.fastq", "w").close()
 # This line creates a "shortest.read.length.fasta" file is it doesn't already exist. It then opens the file for writing.
 fastqShortReads = open("shortest.read.length.fastq", "w+")
 # This for loop writes the reads to the "shortest.read.length.fastq" file
-for i in range(len(reads)):
+for i in range(numErrors):
     # This is how identifiers start in fastq format
     fastqShortReads.write("@R")
     # This adds on to the identifier. We're using the format "@R(length of read)" to identify the reads.
-    fastqShortReads.write(str(len(reads[i])))
+    fastqShortReads.write(str(i))
     # Formatting fastq
     fastqShortReads.write("\n")
     # We write the read to the file
@@ -45,31 +57,31 @@ for i in range(len(reads)):
     # Formatting fastq
     fastqShortReads.write("\n+\n")
     # This is usually a quality score. We will just place tildas since we have created this data.
-    for z in range(len(reads[i])):
+    for z in range(len(readLength)):
         fastqShortReads.write("~")
     # Formatting fastq
     fastqShortReads.write("\n")
 # Closes the file since we're finished.
 fastqShortReads.close()
-fastqSoapSplice = open("soapsplice.fastq", "w").close()
-fastqSoapSplice = open("soapsplice.fastq", "w+")
-for i in range(len(reads)):
-    if len(reads[i]) > 10:
-        # This is how identifiers start in fastq format
-        fastqSoapSplice.write("@R")
-        # This adds on to the identifier. We're using the format "@R(length of read)" to identify the reads.
-        fastqSoapSplice.write(str(len(reads[i])))
-        # Formatting fastq
-        fastqSoapSplice.write("\n")
-        # We write the read to the file
-        fastqSoapSplice.write(reads[i])
-        # Formatting fastq
-        fastqSoapSplice.write("\n+\n")
-        # This is usually a quality score. We will just place tildas since we have created this data.
-        for z in range(len(reads[i])):
-            fastqSoapSplice.write("~")
-        # Formatting fastq
-        fastqSoapSplice.write("\n")
-    else:
-        continue
-fastqSoapSplice.close()
+# fastqSoapSplice = open("soapsplice.fastq", "w").close()
+# fastqSoapSplice = open("soapsplice.fastq", "w+")
+# for i in range(len(reads)):
+#     if len(reads[i]) > 10:
+#         # This is how identifiers start in fastq format
+#         fastqSoapSplice.write("@R")
+#         # This adds on to the identifier. We're using the format "@R(length of read)" to identify the reads.
+#         fastqSoapSplice.write(str(len(reads[i])))
+#         # Formatting fastq
+#         fastqSoapSplice.write("\n")
+#         # We write the read to the file
+#         fastqSoapSplice.write(reads[i])
+#         # Formatting fastq
+#         fastqSoapSplice.write("\n+\n")
+#         # This is usually a quality score. We will just place tildas since we have created this data.
+#         for z in range(len(reads[i])):
+#             fastqSoapSplice.write("~")
+#         # Formatting fastq
+#         fastqSoapSplice.write("\n")
+#     else:
+#         continue
+# fastqSoapSplice.close()
